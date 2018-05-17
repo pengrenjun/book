dab9e0b0ab72d945f415b0e290f77f53e5539392
pengrenjun <809507790@qq.com>
2018-05-17 14:48:32 +0800

    利用视图嵌套结果映射方式查询数据

---
+++ studyReview/src/main/java/MybatisExc/sysvo/SysPrivilegeVo.java
@@ -0,0 +1,47 @@
+package MybatisExc.sysvo;
+
+/**
+ * @Description 权限视图
+ * @Author pengrj
+ * @CreateDate 2018-05-17 下午 2:22
+ * @Version 1.0
+ */
+public class SysPrivilegeVo {
+
+    /**
+     * 权限ID
+     */
+    private Long id;
+    /**
+     * 权限名称
+     */
+    private String privilegeName;
+    /**
+     * 权限URL
+     */
+    private String privilegeUrl;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPrivilegeName() {
+        return privilegeName;
+    }
+
+    public void setPrivilegeName(String privilegeName) {
+        this.privilegeName = privilegeName;
+    }
+
+    public String getPrivilegeUrl() {
+        return privilegeUrl;
+    }
+
+    public void setPrivilegeUrl(String privilegeUrl) {
+        this.privilegeUrl = privilegeUrl;
+    }
+}
